0d48f3c13ed2cdd3efe0e38e23852c694c3d4fa5
GustavoMont <lgmluisgm@gmail.com>
2021-09-20 20:31:02 -0300

    :pencil: make interval changer

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   SafeAreaView, StyleSheet, Text, View, StatusBar,
   TouchableOpacity
@@ -9,6 +8,44 @@ import { Clock, Setas } from './src/Components/Clock';
 import Time from './src/Components/Time'
 
 export default function App() {
+
+  const ciclo = [25, 5, 25, 5, 25, 5, 25, 15]
+  const [resultado, setResultado] = useState('25:00')
+  const [segundos, setSegundos] = useState(ciclo[0])
+  const [run, setRun] = useState(false)
+  const [indexCiclo, setIndexCiclo] = useState(0)
+  let contador = segundos
+
+  const handleCiclo = (atual) =>{
+    if (atual === ciclo.length - 2) {
+      return -1;
+    }
+    return atual + 1
+  }
+  const timer = useCallback(() =>{
+    setTimeout(() => {
+      contador--
+      setSegundos(contador)
+    }, 1000);
+  }, [run])
+  
+  useEffect(() =>{
+    if (contador < 0) {
+      setRun(false)
+      setSegundos(ciclo[indexCiclo+1]);
+      setIndexCiclo(handleCiclo(indexCiclo))
+      return
+    }
+    if (run) {
+      timer()
+    }
+    setResultado(`${Math.floor(segundos/60)}:${segundos%60 < 10 ? '0' + segundos%60 : segundos%60}`)
+  }, [segundos, run])
+
+
+
+
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#135756' }}>
       <StatusBar barStyle="light-content" backgroundColor="#1c8080" />
@@ -27,26 +64,26 @@ export default function App() {
           <Setas positionX={"left: -22.8px;"} rotate={-90}>
             <Entypo name="triangle-down" size={50} color="#9f0"/>
           </Setas>
-          <Time>25:00</Time>
+          <Time>{resultado}</Time>
         </Clock>
       </View>
 
       <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-        <TouchableOpacity>
+        {/* <TouchableOpacity activeOpacity={true}>
           <Entypo name="controller-jump-to-start" size={50} color="#fff" />
-        </TouchableOpacity>
-        <TouchableOpacity>
+        </TouchableOpacity> */}
+        <TouchableOpacity >
           <Entypo name="controller-stop" size={50} color="#fff" />
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Entypo name="controller-play" size={50} color="#fff" />
+        <TouchableOpacity onPress={() => {setRun(true)}}>
+          <Entypo name="controller-play" size={50} color="#fff"></Entypo>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => {setRun(false)}} >
           <Entypo name="controller-paus" size={50} color="#fff" />
         </TouchableOpacity>
-        <TouchableOpacity>
+        {/* <TouchableOpacity>
           <Entypo name="controller-next" size={50} color="#fff" />
-        </TouchableOpacity>
+        </TouchableOpacity> */}
 
       </View>
 
--- src/Components/Time/index.js
+++ src/Components/Time/index.js
@@ -3,7 +3,6 @@ import styled from "styled-components/native";
 const Time = styled.Text`
     color: #fff;
     font-size: 80px;
-    font-family: sans-serif;
     font-weight: 700;
 `
 export default Time;
