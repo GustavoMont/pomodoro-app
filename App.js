545afd181746335c7087d05e3f6cf8075cbda690
GustavoMont <lgmluisgm@gmail.com>
2021-09-21 10:24:16 -0300

    :pencil: move stopTimer and startTimer to Utils file

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import {
 import { Entypo } from '@expo/vector-icons'
 import { Clock, Setas } from './src/Components/Clock';
 import Time from './src/Components/Time'
-import { handleCiclo } from './src/Utils';
+import { handleCiclo, startTimer, stopTimer } from './src/Utils';
 
 export default function App() {
 
@@ -18,31 +18,18 @@ export default function App() {
   const [timer, setTimer] = useState(undefined);
   let contador = segundos
 
-
-  
-  const startTimer = (set) =>{
-    set(setInterval(() => {
-      contador--
-      setSegundos(contador)
-    }, 1000));
-  }
-
-  const stopInterval = (timer) =>{
-      clearInterval(timer)
-}
-
   useEffect(() =>{
     setSegundos(ciclo[indexCiclo])
   }, [indexCiclo])
 
   useEffect(() =>{
     if (contador < 0) {
-      stopInterval(timer)
+      stopTimer(timer)
       setIndexCiclo(handleCiclo(indexCiclo+1, ciclo))
       return
     }
     if (trigger) {
-      startTimer(setTimer)
+      startTimer(setTimer, contador, setSegundos)
       setTrigger(false)
     }
     setResultado(`${Math.floor(segundos/60)}:${segundos%60 < 10 ? '0' + segundos%60 : segundos%60}`)
@@ -76,7 +63,7 @@ export default function App() {
 
       <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
         <TouchableOpacity activeOpacity={true}  onPress={() =>{
-              stopInterval(timer)
+              stopTimer(timer)
               const index = indexCiclo > 0 ? indexCiclo - 1 : 0
               setSegundos(ciclo[index])
               setIndexCiclo(index)
@@ -85,14 +72,14 @@ export default function App() {
         </TouchableOpacity>
         <TouchableOpacity >
           <Entypo name="controller-stop" size={50} color="#fff" onPress={() =>{
-            stopInterval(timer)
+            stopTimer(timer)
             setSegundos(ciclo[indexCiclo])
           }}/>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => {setTrigger(true)}}>
           <Entypo name="controller-play" size={50} color="#fff"></Entypo>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => {stopInterval()}} >
+        <TouchableOpacity onPress={() => {stopTimer(timer)}} >
           <Entypo name="controller-paus" size={50} color="#fff" />
         </TouchableOpacity>
         {/* <TouchableOpacity>
