abd7524bccbc925ebed234f3cdea30c408011ebf
GustavoMont <lgmluisgm@gmail.com>
2021-09-23 00:19:09 -0300

    :tada: add fonts

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-
 import React from 'react';
 import {
   SafeAreaView, StyleSheet, View, StatusBar,
   TouchableOpacity, Dimensions
 } from 'react-native';
 import { Entypo } from '@expo/vector-icons'
+import AppLoading from 'expo-app-loading';
+import { Exo_500Medium, useFonts } from '@expo-google-fonts/exo';
 import { Clock, Setas } from './src/Components/Clock';
 import Time from './src/Components/Time'
 
@@ -12,8 +13,11 @@ const {width, height} = Dimensions.get('screen')
 
 
 export default function App() {
+  const fonts = useFonts({Exo_500Medium})
 
-
+  if (!fonts) {
+    <AppLoading />
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#33711b' }}>
@@ -33,7 +37,7 @@ export default function App() {
           <Setas positionX={"left: -22.8px;"} rotate={-90}>
             <Entypo name="triangle-down" size={50} color="#9f0"/>
           </Setas>
-          <Time>25:00</Time>
+          <Time font={'Exo_500Medium'}>25:00</Time>
         </Clock>
       </View>
 
--- src/Components/Time/index.js
+++ src/Components/Time/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 const Time = styled.Text`
     color: #fff;
     font-size: 80px;
-    font-family: sans-serif;
+    font-family: ${props => props.font};
     font-weight: 700;
 `
 export default Time;
