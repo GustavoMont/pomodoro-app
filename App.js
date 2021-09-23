21da1733897b9a70ba8dbe54e7139f6db55c8ef6
GustavoMont <lgmluisgm@gmail.com>
2021-09-23 13:45:50 -0300

    :pencil: Add font

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {
   SafeAreaView, StyleSheet, View, StatusBar,
-  TouchableOpacity, Dimensions
+  TouchableOpacity, Dimensions, Text
 } from 'react-native';
 import { Entypo } from '@expo/vector-icons'
 import AppLoading from 'expo-app-loading';
-import { Exo_500Medium, useFonts } from '@expo-google-fonts/exo';
+import { Exo_600SemiBold_Italic, useFonts } from '@expo-google-fonts/exo';
 import { Clock, Setas } from './src/Components/Clock';
 import Time from './src/Components/Time'
 
-const {width, height} = Dimensions.get('screen')
+
+let {width, height} = Dimensions.get('screen')
 
 
 export default function App() {
-  const fonts = useFonts({Exo_500Medium})
-
-  if (!fonts) {
-    <AppLoading />
-  }
+  const [fonts] = useFonts({
+    Exo_600SemiBold_Italic
+    })
+    if (!fonts) {
+      return <AppLoading />
+    }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#33711b' }}>
@@ -37,7 +39,7 @@ export default function App() {
           <Setas positionX={"left: -22.8px;"} rotate={-90}>
             <Entypo name="triangle-down" size={50} color="#9f0"/>
           </Setas>
-          <Time font={'Exo_500Medium'}>25:00</Time>
+          <Time font={'Exo_600SemiBold_Italic'} >25:00</Time>
         </Clock>
       </View>
 
